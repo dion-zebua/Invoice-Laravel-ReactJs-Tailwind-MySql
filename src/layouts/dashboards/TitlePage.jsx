224f31cc5e62e90dae6a0e4706c6fc00e2745578
dion-zebua <zebuadbless@gmail.com>
2024-10-04 13:26:52 +0700

    malas

--- src/layouts/dashboards/TitlePage.jsx
+++ src/layouts/dashboards/TitlePage.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function TitlePage(props) {
   const { title } = props;
   return (
-    <div className="bg-gradient-to-r via-30% from-blue-700 via-blue-600 to-blue-700 text-center p-10 pb-20">
-      <h1 className="text-slate-200 inline-block border-b-2 border-b-blue-200">
+    <div className="bg-gradient-to-r via-30% from-blue-700 via-blue-600 to-blue-700 text-center p-10 pb-16">
+      <h1 className="text-slate-200 inline-block relative p-3">
         {title ?? ""}
       </h1>
     </div>
--- src/pages/dashboard/user/Add.jsx
+++ src/pages/dashboard/user/Add.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Layout from "../../../layouts/dashboards/Layout";
 import Container from "../../../layouts/dashboards/Container";
+import FormField from "../../../components/FormField";
+import { InputText } from "primereact/inputtext";
+
 
 export default function Add() {
   return (
     <Layout title="Tambah Pengguna">
-      <Container>test</Container>
+      <Container title="Buat Profil">
+        <FormField>
+          <InputText></InputText>
+        </FormField>
+      </Container>
     </Layout>
   );
 }
