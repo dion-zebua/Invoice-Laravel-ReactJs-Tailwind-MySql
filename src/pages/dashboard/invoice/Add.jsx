05fd95ef5d5407a81979fc705662e06471d6d7f4
dion-zebua <zebuadbless@gmail.com>
2024-10-05 19:57:27 +0700

    malas

--- src/pages/dashboard/invoice/Add.jsx
+++ src/pages/dashboard/invoice/Add.jsx
@@ -9,11 +9,91 @@ import ButtonRight from "../../../components/ButtonRight";
 import { Button } from "primereact/button";
 
 export default function Add() {
-  const [selectedCity, setSelectedCity] = useState(null);
-  const cities = [
+  const [selectedStatus, setSelectedStatus] = useState(null);
+  const [detailPriceInvoice, setDetailPriceInvoice] = useState({
+    sub_total: 0,
+    discount: 0,
+    total: 0,
+    tax: 0,
+    grand_total: 0,
+    down_payment: 0,
+    paid_off: 0,
+  });
+  const [productInvoice, setProductInvoice] = useState([
+    {
+      idRow: 1,
+      name: "",
+      quantity: 1,
+      unit: "",
+      price: "",
+      amount: "",
+    },
+  ]);
+  const [productQuantity, setProductQuantity] = useState([1]);
+  const status = [
     { name: "Belum Lunas", code: "0" },
     { name: "Lunas", code: "1" },
   ];
+
+  const deleteRow = (idRow) => {
+    setProductInvoice(productInvoice.filter((item) => item.idRow !== idRow));
+  };
+
+  const addRow = (e) => {
+    const newId =
+      productInvoice.length > 0
+        ? productInvoice[productInvoice.length - 1].idRow + 1
+        : 1;
+
+    setProductInvoice([
+      ...productInvoice,
+      {
+        idRow: newId,
+        name: "",
+        quantity: 1,
+        unit: "",
+        price: "",
+        amount: "",
+      },
+    ]);
+  };
+
+  const updateQuantity = (idRow, change) => {
+    setProductInvoice((prev) =>
+      prev.map((product) =>
+        // const amount = product.price == null ?
+        product.idRow === idRow
+          ? {
+              ...product,
+              quantity: Math.max(1, parseInt(product.quantity) + change),
+              amount:
+                product.price == ""
+                  ? ""
+                  : Math.max(1, parseInt(product.quantity) + change) *
+                    product.price,
+            }
+          : product
+      )
+    );
+  };
+
+  const handleChangeProduct = (e, idRow) => {
+    const { name, value } = e.target;
+    setProductInvoice((prev) =>
+      prev.map((product) => {
+        if (product.idRow === idRow) {
+          const updatedProduct = { ...product, [name]: value };
+          if (name === "quantity" || name === "price") {
+            updatedProduct.amount =
+              updatedProduct.quantity * updatedProduct.price;
+          }
+
+          return updatedProduct;
+        }
+        return product;
+      })
+    );
+  };
 
   const handleSubmitProfile = (e) => {
     e.preventDefault();
@@ -44,12 +124,12 @@ export default function Add() {
             htmlFor="status"
           />
           <Dropdown
-            value={selectedCity}
-            onChange={(e) => setSelectedCity(e.value)}
+            value={selectedStatus}
+            onChange={(e) => setSelectedStatus(e.value)}
             id="status"
             required
             name="status"
-            options={cities}
+            options={status}
             optionLabel="name"
             className="w-full [&_span]:py-[0.56rem] !h-min"
             checkmark={true}
@@ -123,7 +203,7 @@ export default function Add() {
           <div className="relative overflow-x-auto">
             <table className="w-full text-left rtl:text-right text-gray-500">
               <thead className="text-xs text-gray-700 uppercase bg-gray-100">
-                <tr className="[&_th]:px-6 [&_th]:py-3">
+                <tr className="[&_th]:px-6 [&_th]:py-3 [&_th]:whitespace-nowrap">
                   <th scope="col">Produk / Jasa</th>
                   <th scope="col">Qty</th>
                   <th scope="col">Satuan</th>
@@ -132,74 +212,121 @@ export default function Add() {
                   <th scope="col">Aksi</th>
                 </tr>
               </thead>
-              <tbody className="[&_tr]:bg-white even:[&_tr]:bg-slate-50 hover:[&_tr]:bg-slate-100 [&_td]:border-b [&_td]:px-6 [&_td]:py-3">
-                <tr>
-                  <td className="font-medium text-slate-700">
-                    Travel Jakarta Surabaya
-                  </td>
-                  <td>
-                    <div className="flex items-center">
-                      <button
-                        className="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200"
-                        type="button">
-                        <span className="sr-only">Quantity button</span>
-                        <svg
-                          className="w-3 h-3"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 18 2">
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M1 1h16"
+              <tbody className="[&_tr]:bg-white even:[&_tr]:bg-slate-50 hover:[&_tr]:bg-slate-50 [&_td]:border-b [&_td]:px-6 [&_td]:py-3 [&_td]:whitespace-nowrap [&_td]:min-w-44 [&_input]:!text-sm">
+                {productInvoice.map((product) => (
+                  <tr key={product.idRow}>
+                    <td className="!min-w-60 font-medium text-slate-700">
+                      <InputText
+                        name="name"
+                        onChange={(e) => handleChangeProduct(e, product.idRow)}
+                        value={product.name}
+                      />
+                    </td>
+                    <td>
+                      <div className="flex items-center">
+                        <button
+                          className="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200"
+                          type="button"
+                          onClick={() => updateQuantity(product.idRow, -1)}>
+                          <span className="sr-only">Quantity button</span>
+                          <svg
+                            className="w-3 h-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 2">
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M1 1h16"
+                            />
+                          </svg>
+                        </button>
+                        <div>
+                          <InputText
+                            name="quantity"
+                            onChange={(e) =>
+                              handleChangeProduct(e, product.idRow)
+                            }
+                            min={1}
+                            required
+                            type="number"
+                            value={product.quantity}
                           />
-                        </svg>
-                      </button>
-                      <div>
-                        <input
-                          type="number"
-                          id="quantity"
-                          className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1"
-                          placeholder="1"
-                          required
-                        />
+                        </div>
+                        <button
+                          className="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200"
+                          type="button"
+                          onClick={() => updateQuantity(product.idRow, +1)}>
+                          <span className="sr-only">Quantity button</span>
+                          <svg
+                            className="w-3 h-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 18 18">
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M9 1v16M1 9h16"
+                            />
+                          </svg>
+                        </button>
                       </div>
-                      <button
-                        className="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200"
-                        type="button">
-                        <span className="sr-only">Quantity button</span>
-                        <svg
-                          className="w-3 h-3"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 18 18">
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M9 1v16M1 9h16"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  </td>
-                  <td>Kursi</td>
-                  <td>Rp 180.000</td>
-                  <td>Rp 180.000</td>
-                  <td>
-                    <Button label="Hapus" />
-                  </td>
-                </tr>
+                    </td>
+                    <td>
+                      <InputText
+                        name="unit"
+                        onChange={(e) => handleChangeProduct(e, product.idRow)}
+                        value={product.unit}
+                      />
+                    </td>
+                    <td>
+                      <InputText
+                        type="number"
+                        min={0}
+                        name="price"
+                        onChange={(e) => handleChangeProduct(e, product.idRow)}
+                        value={product.price}
+                      />
+                    </td>
+                    <td>
+                      <InputText
+                        type="number"
+                        min={0}
+                        className="bg-slate-100 border-slate-600 !text-slate-900"
+                        disabled
+                        value={product.amount}
+                      />
+                    </td>
+                    <td>
+                      <Button
+                        type="button"
+                        label="hapus"
+                        size="small"
+                        className="py-1.5"
+                        onClick={(e) => deleteRow(product.idRow)}
+                      />
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
+          <Button
+            type="button"
+            size="small"
+            label="tambah produk"
+            className="py-1.5 mt-5 ml-6"
+            onClick={addRow}
+          />
         </div>
-        <div className="!col-span-full grid grid-cols-8 border-t border-slate-300 mt-12 pt-7 [&>div]:col-span-full md:[&>div]:col-span-3 xl:[&>div]:col-span-2 [&>div]:!col-end-9 [&>div]:flex [&>div]:justify-center [&>div]:items-center [&>div]:gap-x-2 [&_label]:!min-w-28 [&_label]:text-right [&_:is(.disabled,.disabled_label)]:!cursor-not-allowed [&_.disabled_input]:bg-gray-200 [&_.disabled_input]:border-gray-500">
+
+        <div className="!col-span-full grid grid-cols-8 border-t border-slate-300 mt-12 pt-7 [&>div]:col-span-full md:[&>div]:col-span-3 [&>div]:!col-end-9 [&>div]:flex [&>div]:justify-center [&>div]:items-center [&>div]:gap-x-2 [&_label]:!min-w-28 [&_label]:text-right [&_:is(.disabled,.disabled_label)]:!cursor-not-allowed [&_.disabled_input]:bg-gray-200 [&_.disabled_input]:border-gray-500 [&_input]:!text-sm">
           <FormField className="disabled">
             <Label
               className="mb-0"
@@ -207,6 +334,7 @@ export default function Add() {
               htmlFor="sub_total"
             />
             <InputText
+              value={detailPriceInvoice.sub_total}
               disabled
               id="sub_total"
               required
@@ -222,6 +350,7 @@ export default function Add() {
               htmlFor="discount"
             />
             <InputText
+              value={detailPriceInvoice.discount}
               id="discount"
               required
               type="number"
@@ -236,6 +365,7 @@ export default function Add() {
               htmlFor="total"
             />
             <InputText
+              value={detailPriceInvoice.total}
               disabled
               id="total"
               required
@@ -251,6 +381,7 @@ export default function Add() {
               htmlFor="pajak"
             />
             <InputText
+              value={detailPriceInvoice.tax}
               disabled
               id="pajak"
               required
@@ -266,6 +397,7 @@ export default function Add() {
               htmlFor="grand_total"
             />
             <InputText
+              value={detailPriceInvoice.grand_total}
               disabled
               id="grand_total"
               required
@@ -281,6 +413,7 @@ export default function Add() {
               htmlFor="down_payment"
             />
             <InputText
+              value={detailPriceInvoice.down_payment}
               id="down_payment"
               required
               type="number"
@@ -295,6 +428,7 @@ export default function Add() {
               htmlFor="paid_off"
             />
             <InputText
+              value={detailPriceInvoice.paid_off}
               disabled
               id="paid_off"
               required
