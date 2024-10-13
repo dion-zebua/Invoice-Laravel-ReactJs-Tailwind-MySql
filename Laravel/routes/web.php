d83cce6fd5e91931898e8b5476233560f3c0b4cc
dion-zebua <zebuadbless@gmail.com>
2024-10-13 11:17:58 +0700

    malas

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $ipAddress = Request::ip();
+    dd($ipAddress);
 });
