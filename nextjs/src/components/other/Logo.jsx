8a5fb308b49d731bb2545a605d8eb328f05e57ac
dion-zebua <zebuadbless@gmail.com>
2025-03-25 18:31:20 +0700

    malas ngoding, paham 🖐

--- nextjs/src/components/other/Logo.jsx
+++ nextjs/src/components/other/Logo.jsx
@@ -1,16 +1,17 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 
 export default function Logo() {
   return (
     <div className="max-w-36">
-        <Image
-          className="min-w-[96px] md:w-28 xl:w-32"
-          src="/image/invoice.png"
-          width={100}
-          height={25}
-          alt="Logo"
-        />
+      <Image
+        className="min-w-[96px] md:w-28 xl:w-32"
+        src="/image/invoice.png"
+        width={100}
+        height={25}
+        alt={"logo" + process.env.NEXT_PUBLIC_APP_URL_FRONTEND}
+      />
     </div>
   );
 }
