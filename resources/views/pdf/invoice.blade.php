fde27d1815261da4bbcd786a5e67ebd82f6eb36b
dion-zebua <zebuadbless@gmail.com>
2025-05-21 22:00:41 +0700

    malas ngoding, paham 🖐

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -8,9 +8,11 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>#INV-{{ $data->code }}</title>
 
-    {{-- <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> --}}
-    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900"
+        rel="stylesheet">
 
     <style>
         *,
@@ -22,8 +24,6 @@
         }
 
         body {
-            max-width: 800px;
-            margin: 0 auto;
             padding: 60px 0 30px 0;
             font-family: "Poppins", sans-serif !important;
         }
@@ -48,14 +48,12 @@
         }
 
         .status {
-            z-index: 99;
             background: rgb(93, 161, 224);
-            position: relative;
-            left: 50%;
-            transform: translateX(-50%);
-            margin: -60px 0 30px 0;
-            width: 100%;
-            padding: 10px 0px;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 640px;
+            padding: 10px 80px;
             text-transform: capitalize;
             font-style: italic;
             color: #dddddd;
@@ -220,7 +218,7 @@
     <div id="container">
         <div class="status">
             @for ($i = 0; $i < 3; $i++)
-                <span id="status-{{ $i }}" style="padding: 0 20px">
+                <span id="status-{{ $i }}">
                     {{ $data->status }}
                     {{ Carbon\Carbon::parse($data->expire)->isPast() ? ', expired' : '' }}
                 </span>
@@ -316,6 +314,7 @@
                     </tr>
                 </thead>
                 <tbody>
+
                     @foreach ($data['invoiceProducts'] as $item)
                         <tr>
                             <td>{{ $loop->iteration }}</td>
