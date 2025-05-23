0524ed71c2ac2abdd98ccdfc79261976e956aae9
dion-zebua <zebuadbless@gmail.com>
2025-05-23 19:10:28 +0700

    malas ngoding, paham 🖐

--- src/components/other/DefaultBaner.jsx
+++ src/components/other/DefaultBaner.jsx
@@ -5,7 +5,9 @@ export default function DefaultBaner(props) {
   return (
     <div className="bg-gradient-to-tr from-primary to-blue-400 mb-10 md:mb-14 lg:mb-16">
       <div className="bg-[url(/image/star.svg)] bg-right-bottom bg-[size:160%]">
-        <h1 className="title text-white text-center py-10">{pageTitle}</h1>
+        <h1 className="title text-white text-center py-10">
+          {pageTitle ?? "Tanpa Judul"}
+        </h1>
       </div>
     </div>
   );
